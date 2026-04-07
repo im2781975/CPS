e1ddec5d7fa5341528a785902e887b861a041069
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-04-07 21:45:54 +0600

    Update .cpp

--- A/.cpp
+++ A/.cpp
@@ -217,3 +217,168 @@ int subarrXor(int *arr, int n, int m) {
     }
     return cnt;
 }
+void minrange(int n, int k, vector <int> arr) {
+    int mn = 1e9; sort(arr.begin(), arr.end());
+    for(int i = 0; i <= n - k; i++)
+        mn = min(mn, arr[i + k - 1] - arr[i]);
+    cout << mn;
+}
+// given a polynomial‑like weighted sum in powers of 2, decide if it’s divisible by
+//the highest power 2ⁿ; if yes, return the quotient, else return -1
+void isdivisible(int n) {
+    int res = 0, base = 1;
+    for(int i = 0; i <= n; i++) {
+        int x; cin >> x;
+        res += x * base; base *= 2;
+    }
+    int tmp = 1 << n;
+    cout << (res % tmp != 0 ? -1 : res / tmp);
+}
+// implements the FCFS (First‑Come, First‑Serve) CPU scheduling algorithm for n processes 
+const int x = 1e5;
+int finish[x], revtime[x], wait[x], start[x];
+void FCFS(int n) {
+    vector <int> arrive(n), burst(n), id(n);
+    for(int i = 0; i < n; i++) {
+        id[i] = i + 1;
+        cin >> arrive[i] >> burst[i];
+    }
+    vector <int> idx(n);
+    iota(idx.begin(), idx.end(), 0);
+    sort(idx.begin(), idx.end(), [&](int i, int j){
+        if(arr[i] == arr[j]) return id[i] < id[j];
+        else arr[i] < arr[j]; });
+    double avgrevtime = 0, avgwait = 0;
+    for(int pos = 0; pos < n; ++pos) {
+        int i = idx[pos];
+        if(pos == 0) start[i] = arrive[i];
+        else start[i] = finish[idx[pos - 1]];
+        finish[i] = start[i] + burst[i];
+        revtime[i] = finish[i] - arrive[i];
+        wait[i] = revtime[i] - burst[i];
+        avgrevtime += revtime[i];
+        avgwait += wait[i];
+    }
+    cout << avgrevtime / n << " " << avgwait / n << endl;
+}
+// modeling a cyclic‑tournament‑style matching between two strings and counting the total number of wins.
+void cyclicmatch(int n) {
+    int a1 = b1 = c1 = 0;
+    int a2 = b2 = c2 = 0; char ch;
+    for(int 0; i < n; i++) {
+        cin >> ch;
+        if (ch == 'G') a1++;
+        if (ch == 'B') b1++;
+        if (ch == 'K') c1++;
+    }
+    for(int i = 0; i < n; i++) {
+        cin >> ch;
+        if (ch == 'G') a2++;
+        if (ch == 'B') b2++;
+        if (ch == 'K') c2++;
+    }
+    cout << min(a1, c2) + min(c1, b2) + min(b1, a2) << endl;
+}
+// Computes the minimum possible “maximum gap” in a cyclic‑style setting
+void minpossible(int n, int k, vector <int> arr) {
+    sort(arr.begin(), arr.end());
+    int res = arr[n - 1] - arr[0];
+    for(int i = 0; i < n - 1; i++)
+        res = min(res, arr[i] + k - arr[i - 1]);
+    cout << res << " ";
+}
+// finds the smallest possible maximum difference over any contiguous subarray of length m after sorting the array
+void minpossible(int n, int k, vector <int> arr) {
+    sort(arr.begin(), arr.end());
+    int res = arr[k - 1] - arr[0];
+    for(int i = k - 1; i < n; ++i) 
+        res = min(res, arr[i] - arr[i - k + 1])
+    cout << res << " ";
+}
+// Sum‑of‑max and sum‑of‑min over 3 triplets, repeated 3 times with separate output each time.
+void sumOf(int n) {
+    while(n--) {
+        int mx = 0, mn = 0;
+        for(int i = 0; i < 3; i++) {
+            int x, y, z; cin >> x >> y >> z;
+            mx += max({x, y, z});
+            mn += min({x, y, z});
+        }
+        cout << mx << " " << mn << endl;
+    }
+}
+// Given a budget x and three item costs arr[0], arr[1], arr[2], it computes the maximum number of items you can buy
+void maxbuy(int budget, vector <int> arrint
+    sort(arr.begin(), arr.end());
+    int cost = arr[0] + arr[1] + arr[2];
+    int tmp = budget / cost;
+    int res = tmp * 3;
+    int rem = budget - tmp * cost;
+    for(int i = 0; i < 3; i++) {
+        if(rem >= arr[i])
+            rem -= arr[i], res++;
+    }
+    cout << res << ' ';
+}
+// simplifies the ratio [a : b] by their GCD, or indicates that they are already coprime.
+void factorization(int a, int b) {
+    int x = __gcd(a, b);
+    if(x > 1) cout << a / x << " " << b / x << endl;
+    else cout << "NO";
+}
+// counts a “number of even positions” in [l, r] 
+void cntEven(int l, int r, int k) {
+    if(l == r) 
+        cout << (l == 1 ? "NO" : "YES") << endl, return 0;
+    int cnt = (r - l) / 2;
+    if(l % 2 != 0 || r % 2 != 0) cnt++;
+    cout << (cnt > k ? "No" : "Yes") << endl;
+}
+void sumOf(int n, int k, vector <int> arr, vector <int> tmp) {
+    vector <pair <int, int>> vec;
+    for(int i = 0; i < n; i++) 
+        vec.push_back({arr[i], tmp[i]});
+    sort(vec.begin(), vec.end());
+    for(int i = 0; i < n; ++i) {
+        if(vec[i].first <= k) k += vec[i].second;
+        else break;
+    }
+    cout << k << " ";
+}
+// Do the two arrays share exactly half of the elements of the first array
+void Ishalfed(int n, int m, vector <int> arr, vector <int> tmp) {
+    sort(arr, arr + n); sort(tmp, tmp + m);
+    int cnt = 0;
+    for(int i = 0, j = 0; i < n, j < m;) {
+        if(arr[i] == tmp[j]) {
+            i++; j++; cnt++;
+        }
+        else if(arr[i] < tmp[j]) i++;
+        else j++;
+    }
+    (cnt == n / 2) ? cout << "Yes" : cout << "No";
+}
+// count how many integer starting values x exist so that x + every prefixsum stays in [lower, upper].
+int cntdig(vector <int> vec, int lower, int upper) {
+    int mini = maxi = sum = 0;
+    for(int x : vec) {
+        sum += x; maxi = max(sum, maxi);
+        mini = min(sum, mini);
+    } /*
+    int cnt = 0;
+    for(int i = lower; i <= upper; i++) {
+        if(i + mini < lower || i + maxi > upper) continue;
+        else cnt++;
+    } return cnt; */
+    int l = lower - mini, r = upper - maxi;
+    if(r < lower || l > upper) return 0;
+    return min(upper, r) - max(lower, l) + 1;
+}
+// removes every 3rd highest price as a discount.Prints the final minimized cost after discounts.
+void mincost(int n, vector <int> vec) {
+    sort(vec.rbegin(), vec.rend());
+    int sum = 0;
+    for(int x : vec) sum += x;
+    for(int i = 2; i < vec.size(); i += 3) sum -= vec[i];
+    cout << sum;
+}
