e3a314983c37dc6b8b5a651edbf387959b345e5c
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-04-07 16:42:46 +0600

    Update .cpp

--- CFS/.cpp
+++ CFS/.cpp
@@ -1,4 +1,84 @@
+//https://codeforces.com/contest/200/problem/B
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    double avg,sum=0;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+    for(int i=0; i<n; i++)
+    {
+        sum+=arr[i];
+    }
+    avg=(double)sum/(double)n;
+    cout<<avg;
+}
+//https://codeforces.com/problemset/problem/1542/A
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n, odd = 0, even = 0;
+        cin >> n;
+        int arr[2 * n];
+        for(int i = 0; i < 2 * n; i++)
+        {
+            cin >> arr[i];
+        }
+        for(int i = 0; i < 2 * n; i++)
+        {
+            if(arr[i] % 2 == 0)
+            {
+                even++;
+            }
+            else
+            {
+                odd++;
+            }
+        }
+        if(even == odd)
+            cout << "Yes\n";
+        else
+            cout << "No\n";
+    }
+    return 0;
+}
+// https://codeforces.com/problemset/problem/1807/A
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        if (a + b == c)
+        {
+            cout << "+\n";
+        }
+        else
+        {
+            cout << "-\n";
+        }
+    }
+
+    return 0;
+}
 // 490A
 void teamOlympiad(int n) {
     vector <vector <int>> team(3);
