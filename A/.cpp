a67e43d0736e23d3392d5c681f48f9141ddffc6d
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-04-09 21:46:27 +0600

    Update .cpp

--- A/.cpp
+++ A/.cpp
@@ -54,3 +54,45 @@ void cntappear(int n, int *arr) {
         if (1 <= r[i] && r[i] <= 100) distinct.insert(r[i]);
     } cout << distinct.size();
 }
+// select a specific subset of elements from arr to form a sum in a pattern related to n % 3
+void formsum(int n, int *arr) {
+    sort(arr, arr + n);
+    int res = 0, skip = 0;
+    if(n % 3 == 1) {
+        res += arr[0]; skip = 1;
+    }
+    else if(n % 3 == 2) {
+        res += arr[0] + arr[1]; skip = 2;
+    } 
+    for(int i = 0; i < n / 3; i++) {
+        int p = skip + 1 + 3 * i;
+        res += arr[p] + arr[p + 1]:
+    }
+}
+// prints the first letter of each word in the line 
+void firstchar(string str) {
+    cout << str[0];
+    for(int i = 0; i < str.length(); i++) {
+        if(str[i] == ' ') cout << str[i + 1];
+    }
+}
+// Given an integer n, it constructs and prints a special integer sequence
+// that “encodes” n using groups of 3s and 3, -2 pairs.
+void encode(int n) {
+    vector <int> arr;
+    for(int i = 0; i < n / 3; i++) arr.push_back(3);
+    // For each leftover element (0, 1, or 2), add (3, -2)
+    for(int i = 0; i < n % 3; i++) {
+        arr.push_back(3); arr.push_back(-2);
+    }
+    cout << arr.size() << endl;
+    for(int x : arr) cout << x << " ";
+}
+void powertwo(int64_t n, int64_t mx) {
+    if(n > 26) {
+        cout << mx; return 0;
+    }
+    int64_t power = 1;
+    for (int i = 0; i < n; ++i) power *= 2;
+    cout << x % power;
+}
