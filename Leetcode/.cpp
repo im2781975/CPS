30765f8782323442a286d029582990ebaf88b71e
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-04-12 23:10:10 +0600

    Create .cpp

---
+++ Leetcode/.cpp
@@ -0,0 +1,50 @@
+https://leetcode.com/problems/number-of-provinces/
+class solution {
+    public: // 547
+    vector<int> parent, rank;
+    int find(int x) {
+        if (parent[x] != x) parent[x] = find(parent[x]);
+        return parent[x];
+    }
+    void unite(int x, int y) {
+        int px = find(x), py = find(y);
+        if (px == py) return;
+        if (rank[px] < rank[py]) parent[px] = py;
+        else if (rank[px] > rank[py]) parent[py] = px;
+        else { parent[py] = px; rank[px]++; }
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        parent.resize(n); rank.resize(n, 0);
+        for (int i = 0; i < n; i++) parent[i] = i;
+        
+        for (int i = 0; i < n; i++)
+            for (int j = i + 1; j < n; j++)
+                if (isConnected[i][j]) unite(i, j);
+        
+        unordered_set<int> roots;
+        for (int i = 0; i < n; i++) roots.insert(find(i));
+        return roots.size();
+    }
+};
+https://leetcode.com/problems/redundant-connection/
+class solution {
+    public: // 684
+    vector <int> parent;
+    int find(int x) {
+        return parent[x] == x ? x : parent[x] = find(parent[x]);
+    }
+    vector <int> findRedundantConnection(vector <vector <int>> &edges) {
+        int n = edges.size();
+        parent.resize(n + 1);
+        for(int i = 0; i <= n; i++) parent[i] = i;
+        for(auto edg : edges) {
+            int u = edg[0], v = edg[1];
+            int pu = find(u), pv = find(v);
+            if(pu == pv) return edg;
+            parent[pu] = pv;
+        } return {};
+    }
+};
+
+
